58dbc89091417891aba302ee63838ded89e8b270
Pavel Tschors <pavel.tschors@softensity.com>
2014-06-14 22:50:32 +0300

    datacollector update

--- app/src/main/java/org/bitbucket/rocketracoons/deviceradar/utility/DataCollector.java
+++ app/src/main/java/org/bitbucket/rocketracoons/deviceradar/utility/DataCollector.java
@@ -10,6 +10,12 @@ import org.bitbucket.rocketracoons.deviceradar.RadarApplication;
 import org.bitbucket.rocketracoons.deviceradar.model.DeviceData;
 import org.bitbucket.rocketracoons.deviceradar.model.ExtendedDeviceData;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.text.DecimalFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Stenopolz on 14.06.2014.
  */
@@ -41,5 +47,53 @@ public class DataCollector {
         Point size = new Point();
         display.getSize(size);
         return String.valueOf(size.x) + "x" + String.valueOf(size.y);
+    }
+
+    public String getTotalRAM() {
+
+        RandomAccessFile reader = null;
+        String load = null;
+        DecimalFormat twoDecimalForm = new DecimalFormat("#.##");
+        double totRam = 0;
+        String lastValue = "";
+        try {
+            reader = new RandomAccessFile("/proc/meminfo", "r");
+            load = reader.readLine();
+
+            // Get the Number value from the string
+            Pattern p = Pattern.compile("(\\d+)");
+            Matcher m = p.matcher(load);
+            String value = "";
+            while (m.find()) {
+                value = m.group(1);
+                // System.out.println("Ram : " + value);
+            }
+            reader.close();
+
+            totRam = Double.parseDouble(value);
+            // totRam = totRam / 1024;
+
+            double mb = totRam / 1024.0;
+            double gb = totRam / 1048576.0;
+            double tb = totRam / 1073741824.0;
+
+            if (tb > 1) {
+                lastValue = twoDecimalForm.format(tb).concat(" TB");
+            } else if (gb > 1) {
+                lastValue = twoDecimalForm.format(gb).concat(" GB");
+            } else if (mb > 1) {
+                lastValue = twoDecimalForm.format(mb).concat(" MB");
+            } else {
+                lastValue = twoDecimalForm.format(totRam).concat(" KB");
+            }
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            //Streams.close(reader);
+        }
+
+        return lastValue;
     }
 }
