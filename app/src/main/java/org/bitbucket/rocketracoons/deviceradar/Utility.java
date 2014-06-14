63a65ee94fb8d8141bdbff758b4f6775af9c9179
Igor.Veyalkin <instub@gmail.com>
2014-06-14 12:11:19 +0300

    add wifi info retreiveing

---
+++ app/src/main/java/org/bitbucket/rocketracoons/deviceradar/Utility.java
@@ -0,0 +1,15 @@
+package org.bitbucket.rocketracoons.deviceradar;
+
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+
+/**
+ * Created by Igor.Veyalkin on 14.06.2014.
+ */
+public class Utility {
+    public static final WifiInfo getWifiInfo(Context context) {
+        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        return wifiManager.getConnectionInfo();
+    }
+}
