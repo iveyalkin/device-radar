dfbb01dd456b4cb693368ac14695b07486cb1f6b
Igor.Veyalkin <instub@gmail.com>
2014-06-14 16:35:06 +0300

    make models serializable

--- app/src/main/java/org/bitbucket/rocketracoons/deviceradar/model/Device.java
+++ app/src/main/java/org/bitbucket/rocketracoons/deviceradar/model/Device.java
@@ -6,5 +6,7 @@ import com.google.gson.annotations.SerializedName;
  * Created by Igor.Veyalkin on 14.06.2014.
  */
 public class Device extends ExtendedDeviceData {
+    private static final long serialVersionUID = -1886750723800451640L;
+
     @SerializedName("date") public String updateDate;
 }
