c292a218bf3fac9c895da55509c18da17aba9bbd
Igor.Veyalkin <instub@gmail.com>
2014-06-14 17:32:36 +0300

    add push notification messages

---
+++ app/src/main/java/org/bitbucket/rocketracoons/deviceradar/model/Message.java
@@ -0,0 +1,24 @@
+package org.bitbucket.rocketracoons.deviceradar.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by Igor.Veyalkin on 14.06.2014.
+ */
+public class Message implements Serializable {
+    private static final long serialVersionUID = 672135616360560604L;
+
+    @SerializedName("date") public Date date;
+    @SerializedName("author") public String authorName;
+    @SerializedName("type") public Type type;
+    @SerializedName("text") public String message;
+
+    public enum Type {
+        INBOUND, OUTBOUND
+    }
+
+    private Message() {}
+}
