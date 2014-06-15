bab8a26ba14fb1b12af16d61e3e2d6399ca15263
Igor.Veyalkin <instub@gmail.com>
2014-06-15 06:08:06 +0300

    update send push

--- app/src/main/java/org/bitbucket/rocketracoons/deviceradar/network/model/PushNotification.java
+++ app/src/main/java/org/bitbucket/rocketracoons/deviceradar/network/model/PushNotification.java
@@ -5,6 +5,7 @@ import com.google.gson.annotations.SerializedName;
 import org.bitbucket.rocketracoons.deviceradar.model.Message;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * Created by Igor.Veyalkin on 14.06.2014.
@@ -12,8 +13,9 @@ import java.io.Serializable;
 public class PushNotification implements Serializable {
     private static final long serialVersionUID = -8851064191604530172L;
 
-    @SerializedName("authorId") public String authorId;
-    @SerializedName("message") public Message message;
+    @SerializedName("author") public String authorId;
+    @SerializedName("text") public Message message;
+    @SerializedName("date") public Date date;
 
     private PushNotification() {}
 }
