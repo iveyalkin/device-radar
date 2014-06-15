e077e4b678b59b70ebb8bc3c800a8784d5a46aa7
Pavel Tschors <pavel.tschors@softensity.com>
2014-06-15 06:14:12 +0300

    Revert "update send push"
    This reverts commit bab8a26ba14fb1b12af16d61e3e2d6399ca15263.

--- app/src/main/java/org/bitbucket/rocketracoons/deviceradar/network/model/PushNotification.java
+++ app/src/main/java/org/bitbucket/rocketracoons/deviceradar/network/model/PushNotification.java
@@ -5,7 +5,6 @@ import com.google.gson.annotations.SerializedName;
 import org.bitbucket.rocketracoons.deviceradar.model.Message;
 
 import java.io.Serializable;
-import java.util.Date;
 
 /**
  * Created by Igor.Veyalkin on 14.06.2014.
@@ -13,9 +12,8 @@ import java.util.Date;
 public class PushNotification implements Serializable {
     private static final long serialVersionUID = -8851064191604530172L;
 
-    @SerializedName("author") public String authorId;
-    @SerializedName("text") public Message message;
-    @SerializedName("date") public Date date;
+    @SerializedName("authorId") public String authorId;
+    @SerializedName("message") public Message message;
 
     private PushNotification() {}
 }
